9996f99ec4e228017b2e090957ad556fca3e2dc5
Marko Gargenta <marko@marakana.com>
2012-11-01 16:44:12 -0700

    Adding CompassView

---
+++ Compass/src/com/marakana/android/compass/CompassView.java
@@ -0,0 +1,20 @@
+package com.marakana.android.compass;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+public class CompassView extends ImageView {
+
+	public CompassView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		super.onDraw(canvas);
+	}
+
+	
+}
--- Compass/src/com/marakana/android/compass/MainActivity.java
+++ Compass/src/com/marakana/android/compass/MainActivity.java
@@ -1,10 +1,17 @@
 package com.marakana.android.compass;
 
+import java.io.IOException;
+import java.util.List;
+
 import android.app.Activity;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.GpsSatellite;
+import android.location.GpsStatus;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
@@ -17,7 +24,9 @@ public class MainActivity extends Activity {
 	private static final long MIN_TIME = 10000; // milliseconds
 	private static final float MIN_DISTANCE = 10; // meters
 	private TextView locationOutput, sensorOutput;
-	private LocationManager locationMmanager;
+	private LocationManager locationManager;
+	private Geocoder geocoder;
+	private GpsStatus gpsStatus;
 	private SensorManager sensorManager;
 	private Sensor sensor;
 
@@ -28,8 +37,9 @@ public class MainActivity extends Activity {
 		locationOutput = (TextView) findViewById(R.id.location_output);
 		sensorOutput = (TextView) findViewById(R.id.sensor_output);
 
-		locationMmanager = (LocationManager) getSystemService(LOCATION_SERVICE);
-		Location location = locationMmanager.getLastKnownLocation(PROVIDER);
+		geocoder = new Geocoder(this);
+		locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+		Location location = locationManager.getLastKnownLocation(PROVIDER);
 		updateLocation(location);
 
 		// Sensor
@@ -41,8 +51,9 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onResume() {
 		super.onResume();
-		locationMmanager.requestLocationUpdates(PROVIDER, MIN_TIME,
+		locationManager.requestLocationUpdates(PROVIDER, MIN_TIME,
 				MIN_DISTANCE, locationListener);
+		locationManager.addGpsStatusListener(gpsListener);
 		sensorManager.registerListener(sensorListener, sensor,
 				SensorManager.SENSOR_DELAY_NORMAL);
 	}
@@ -50,7 +61,9 @@ public class MainActivity extends Activity {
 	@Override
 	protected void onPause() {
 		super.onPause();
-		locationMmanager.removeUpdates(locationListener);
+		locationManager.removeUpdates(locationListener);
+		locationManager.removeGpsStatusListener(gpsListener);
+		sensorManager.unregisterListener(sensorListener);
 	}
 
 	private final LocationListener locationListener = new LocationListener() {
@@ -83,17 +96,49 @@ public class MainActivity extends Activity {
 			return;
 		}
 
-		locationOutput.setText(String.format("Lat: %.2f\nLong: %.2f",
+		locationOutput.setText("");
+
+		// Geocode the location
+		if (Geocoder.isPresent()) {
+			List<Address> addresses;
+			try {
+				addresses = geocoder.getFromLocation(location.getLatitude(),
+						location.getLongitude(), 1);
+				if (!addresses.isEmpty()) {
+					Address address = addresses.get(0);
+					locationOutput.append(String.format("\n%s\n%s\n%s, %s",
+							address.getAddressLine(0),
+							address.getAddressLine(1), address.getLocality(),
+							address.getAdminArea()));
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		// Update the location
+		locationOutput.append(String.format("\nLat: %.2f Long: %.2f",
 				location.getLatitude(), location.getLongitude()));
 
 		if (location.hasAltitude())
-			locationOutput.append(String.format("\nAlt: %.2f",
+			locationOutput.append(String.format(" Alt: %.2f",
 					location.getAltitude()));
 
 		locationOutput.append(String.format("\n%s via %s", DateUtils
 				.getRelativeTimeSpanString(location.getTime()).toString(),
 				PROVIDER));
+
 	}
+
+	private GpsStatus.Listener gpsListener = new GpsStatus.Listener() {
+		@Override
+		public void onGpsStatusChanged(int event) {
+			gpsStatus = locationManager.getGpsStatus(null);
+			((TextView) findViewById(R.id.satellites_output))
+					.setText("\nSatellites time-to-first-fix: "
+							+ gpsStatus.getTimeToFirstFix() + "ms");
+		}
+	};
 
 	private SensorEventListener sensorListener = new SensorEventListener() {
 
@@ -113,7 +158,8 @@ public class MainActivity extends Activity {
 			return;
 		}
 
-		sensorOutput.setTag(String.format("Azimuth: %s\nPitch: %s\nRoll: %s",
-				event.values[0], event.values[0], event.values[0]));
+		sensorOutput.setText(String.format(
+				"Azimuth: %.2f\nPitch: %.2f\nRoll: %.2f", event.values[0],
+				event.values[0], event.values[0]));
 	}
 }
