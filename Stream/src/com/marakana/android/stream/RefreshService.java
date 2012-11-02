345ca85f2c16be51dc7eb8064ade294e665366e3
Marko Gargenta <marko@marakana.com>
2012-11-02 12:12:37 -0700

    Added RefreshService

---
+++ Stream/src/com/marakana/android/stream/RefreshService.java
@@ -0,0 +1,19 @@
+package com.marakana.android.stream;
+
+import android.app.IntentService;
+import android.content.Intent;
+
+public class RefreshService extends IntentService {
+	private static final String TAG = "Stream-RefreshService";
+
+	public RefreshService() {
+		super(TAG);
+	}
+
+	@Override
+	protected void onHandleIntent(Intent intent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
